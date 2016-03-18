cff9f50873649cca8fba9b84e6833295c4aa3c3c
lemon5920 <lemon5920@gmail.com>
2016-03-18 14:29:00 +0800

    fractal triangle

--- pen0318/TestPen.java
+++ pen0318/TestPen.java
@@ -1,6 +1,7 @@
 import java.lang.*;
 
 public class TestPen {
+
 public static Pen p = new Pen();
 
     public static void triangle(int x, int y, int h) {
@@ -10,7 +11,19 @@ public static Pen p = new Pen();
         p.runTo(x, y);
     }
 
+    public static void fractalTriangle(int x, int y, int h) {
+        if(h <= 1) {
+            p.flyTo(x, y);
+            p.runTo(x, y);
+            return;
+        }
+        fractalTriangle(x, y, h/2);
+        fractalTriangle((int)(x + h / Math.sqrt(3) / 2 + 0.5), y+h/2, h/2);
+        fractalTriangle((int)(x - h / Math.sqrt(3) / 2 + 0.5), y+h/2, h/2);
+
+    }
+
     public static void main(String[] argv) {
-       triangle(400, 50, 512); 
+       fractalTriangle(400, 50, 512); 
     }
 }
