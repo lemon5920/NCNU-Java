9022e0c54638c7d3f247b9d7ad3f6479d8f28d7f
lemon5920 <lemon5920@gmail.com>
2016-03-18 14:12:39 +0800

    gitignore add *.swp, TestPen make a big triangle

--- pen0318/TestPen.java
+++ pen0318/TestPen.java
@@ -1,11 +1,16 @@
+import java.lang.*;
+
 public class TestPen {
+public static Pen p = new Pen();
+
+    public static void triangle(int x, int y, int h) {
+        p.flyTo(x, y);
+        p.runTo((int)(x + h / Math.sqrt(3) + 0.5), y + h );
+        p.runTo((int)(x - h / Math.sqrt(3) + 0.5), y + h );
+        p.runTo(x, y);
+    }
+
     public static void main(String[] argv) {
-        Pen p = new Pen();
-        p.flyTo(50, 50);
-        p.runTo(150, 50);
-        p.runTo(150, 150);
-        p.runTo(50, 150);
-        p.runTo(50, 50);
-        
+       triangle(400, 50, 512); 
     }
 }
